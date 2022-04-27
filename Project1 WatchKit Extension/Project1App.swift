905f211840798a5302dde5dead8d1bd9be34a404
Olivier Van hamme <nicefiction@gmail.com>
2022-04-27 22:54:15 +0200

    Initial Commit

---
+++ Project1 WatchKit Extension/Project1App.swift
@@ -0,0 +1,19 @@
+//
+//  Project1App.swift
+//  Project1 WatchKit Extension
+//
+//  Created by Olivier Van hamme on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
